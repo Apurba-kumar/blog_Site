aa79e901a8cc0142d304393c1d4c69fa69769928
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-09-15 22:38:24 +0600

    Resource controller

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,19 @@ Route::delete('/blog/{post}',[BlogController::class,'destroy'])->name('blog.dest
 
 Route::post('/blogs',[BlogController::class,'store'])->name('blog.store');
 
+// Category resource controller
+Route::resource('/categories', CategoryController::class);
+
+// The resource controller above under the hood.
+
+// Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+// Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+// Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+// Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+// Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+// Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+// Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
